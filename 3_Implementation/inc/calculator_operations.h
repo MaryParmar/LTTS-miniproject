4d83ec02e09981930fcc2542f0a776fbfe8746c7
MaryParmar <80715594+MaryParmar@users.noreply.github.com>
2021-04-09 16:29:59 +0530

    Update calculator_operations.h

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -12,24 +12,24 @@
 
 /**
 *  adds the operand1 and operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
+* @mary[in] operand1 
+* @mary[in] operand2 
 * @return Result of operand1 + operand2
 */
 int add(int operand1, int operand2);
 
 /**
 *  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
+* @mary[in] operand1 
+* @mary[in] operand2 
 * @return Result of operand1 - operand2
 */
 int subtract(int operand1, int operand2);
 
 /**
 *  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
+* @mary[in] operand1 
+* @mary[in] operand2 
 * @return Result of operand1 * operand2
 */
 int multiply(int operand1, int operand2);
@@ -37,8 +37,8 @@ int multiply(int operand1, int operand2);
 
 /**
 * divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
+* @mary[in] operand1 
+* @mary[in] operand2 
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
 */
